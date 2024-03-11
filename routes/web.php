fc324382656fdd9d9c25a01467d2d9c0ec612e2f
Maxou305 <maxime.chazard@le-campus-numerique.fr>
2024-03-11 11:18:11 +0100

    routes ok

---
+++ routes/web.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+Route::get('/meows', function () {
+    return 'liste de messages';
+});
+Route::get('/meow/{id}', function (string $id) {
+    return 'Message ' . $id;
+});
