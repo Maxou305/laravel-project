7dc859264ba08fcec3170f591197ae2e2814a8df
Maxou305 <maxime.chazard@le-campus-numerique.fr>
2024-03-13 09:48:02 +0100

    navbar added

---
+++ resources/views/components/navbar.blade.php
@@ -0,0 +1,17 @@
+<nav class="navbar navbar-expand-lg navbar-light bg-light px-4">
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup"
+            aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+        <div class="navbar-nav">
+            <a class="nav-item nav-link" href="/">
+                <img style="width: 30px" src="https://cdn-icons-png.flaticon.com/512/5808/5808039.png" alt="Home">
+            </a>
+            <a class="nav-item nav-link" href="/meows">
+                <img style="width: 30px" src="https://cdn-icons-png.flaticon.com/512/1391/1391034.png" alt="Meows">
+            </a>
+        </div>
+    </div>
+</nav>
+
