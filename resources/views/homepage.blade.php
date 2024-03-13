84ce96d0c6af5bbf31ed260d5074da343f974ef8
Maxou305 <maxime.chazard@le-campus-numerique.fr>
2024-03-13 09:48:23 +0100

    homepage and meows list reviewed

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -1,8 +1,6 @@
-<div>
-    <h1>Page d'accueil OMG</h1>
-    <h2>Liste des users</h2>
-    <ul>
-        @foreach ($users as $user)
-            <li>{{ $user->name }}</li>
-    @endforeach
-</div>
+<x-layout>
+    <h1>Page d'accueil</h1>
+    <div class="list-group">
+        <a href="/meows">TOTO OBRIGADO</a>
+    </div>
+</x-layout>
--- resources/views/meows-list.blade.php
+++ resources/views/meows-list.blade.php
@@ -1,10 +1,20 @@
-<div>
-    <h1>Meows List</h1>
-    @foreach ($meows as $meow)
-        <h2 style="color: forestgreen">{{$meow->content}}</h2>
-        <p style="color: darkblue">Author : {{ $meow->user->name}}</p>
-        @foreach($meow->comments as $comment)
-            <p style="color: darkred">Comment : {{ $comment->content}} - {{$comment->user->name}}</p>
+<x-layout>
+    <div style="padding: 20px; display: flex; flex-direction: column; gap: 5px">
+        <h1>Meows List</h1>
+        @foreach ($meows as $meow)
+            <div class="card text-bg-dark mb-3">
+                <div class="card-header" style="gap: 10px">
+                    <img src="https://cdn-icons-png.flaticon.com/512/616/616430.png" alt="meow icon"
+                         style="width: 25px">
+                    {{ $meow->user->name }}
+                </div>
+                <div class="card-body">
+                    <h5 class="card-title">{{ $meow->content}}</h5>
+                    @foreach($meow->comments as $comment)
+                        <p class="card-text">{{ $comment->content }} - {{ $comment->user->name }}</p>
+                    @endforeach
+                </div>
+            </div>
         @endforeach
-    @endforeach
-</div>
+    </div>
+</x-layout>
