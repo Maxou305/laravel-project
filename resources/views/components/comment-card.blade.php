e8dfeba0dee1080f5b29fc76ff63565da5e66fb7
Maxou305 <maxime.chazard@le-campus-numerique.fr>
2024-03-14 11:53:23 +0100

    delete button added but not working

--- resources/views/components/comment-card.blade.php
+++ resources/views/components/comment-card.blade.php
@@ -1,13 +1,6 @@
-{{--<div class="card text-bg-warning mb-3">--}}
-{{--    <div class="card-header">{{$comment->user->name}}</div>--}}
-{{--    <div class="card-body">--}}
-{{--        <p class="card-text">{{$comment->content}}</p>--}}
-{{--    </div>--}}
-{{--    <div class="blockquote-footer fst-italic mt-1">{{$comment->created_at}}</div>--}}
-{{--</div>--}}
-
 <div
-    class="mt-5  block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+    class="mt-5 block p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
     <p class="font-normal text-gray-700 text-xl">{{$comment->content}}</p>
-    <p class="text-2xl font-bold tracking-tight text-gray-900 italic">Author : {{$comment->user->name}}</p>
+    <p class="tracking-tight text-gray-900 italic">Author : {{$comment->user->name}}</p>
+    <x-danger-button wire:click="delete({{$comment->id}})" class="mt-2">Delete</x-danger-button>
 </div>
