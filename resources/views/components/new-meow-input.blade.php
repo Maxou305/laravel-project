1dec88b42ca48d3898686da85cca7f8c0b7b9c5d
Maxou305 <maxime.chazard@le-campus-numerique.fr>
2024-03-14 11:53:48 +0100

    new input for new meow added

---
+++ resources/views/components/new-meow-input.blade.php
@@ -0,0 +1,8 @@
+<div class="flex gap-4">
+    <input type="search" id="default-search"
+           class="w-full block p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+           placeholder="New meow"/>
+    <button>
+        <img src="https://www.svgrepo.com/show/457745/send.svg" alt="" style="width: 50px">
+    </button>
+</div>
